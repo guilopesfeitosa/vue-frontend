eaf0c83199b298927b211d551b57562947b9f6d4
Guilherme Lopes <68930185+guilopesfeitosa@users.noreply.github.com>
2024-11-14 09:50:09 -0300

    feat: initial setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <p>header component</p>
+  </header>
+
+  <main>
+    <p>main component</p>
+  </main>
+</template>
+
+<style scoped></style>
